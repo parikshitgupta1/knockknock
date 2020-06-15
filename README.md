1884732c3a9394eefbfe27f3b6a8c71cb877be14
Parikshit Gupta <45290746+parikshitgupta1@users.noreply.github.com>
2020-06-16 01:12:44 +0530

    final version
    ready to be deployed

---
+++ README.md
@@ -0,0 +1,5 @@
+# KnockKnock
+A Knock Knock chat application, built with love, for fun.
+
+# Deployed Via Netlify
+[![Netlify Status](https://api.netlify.com/api/v1/badges/4689abec-e0d9-479c-8aca-baadfae75b32/deploy-status)](https://app.netlify.com/sites/knock-knock/deploys) https://knock-knock.netlify.app
---
+++ index.js
@@ -0,0 +1,650 @@
+var jokes = [
+    {
+    "id": "1",
+    "firstResponse": "Etch",
+    "secondResponse": "Bless you, friend."
+},
+{
+    "id": "2",
+    "firstResponse": "Robin",
+    "secondResponse": "Robin you, give me all your money!"
+},
+{
+    "id": "3",
+    "firstResponse": "Mustache",
+    "secondResponse": "I mustache you a question, but I’ll shave it for later."
+},
+{
+    "id": "4",
+    "firstResponse": "Tank",
+    "secondResponse": "You’re welcome."
+},
+{
+    "id": "5",
+    "firstResponse": "Voodoo",
+    "secondResponse": "Voodoo you think you are, asking me so many questions?"
+},
+{
+    "id": "6",
+    "firstResponse": "Spell",
+    "secondResponse": "Okay, okay: W. H. O."
+},
+{
+    "id": "7",
+    "firstResponse": "Candice",
+    "secondResponse": "Candice door open, or what?"
+},
+{
+    "id": "8",
+    "firstResponse": "Stopwatch",
+    "secondResponse": "Stopwatch you’re doing and pay attention!"
+},
+{
+    "id": "9",
+    "firstResponse": "To",
+    "secondResponse": "It’s to whom."
+},
+{
+    "id": "10",
+    "firstResponse": "Dewey",
+    "secondResponse": "Dewey have to use a condom every time?"
+},
+{
+    "id": "11",
+    "firstResponse": "Honey bee",
+    "secondResponse": "Honey bee a dear and open up, would you?"
+},
+{
+    "id": "12",
+    "firstResponse": "Lettuce",
+    "secondResponse": "Lettuce in, it’s cold out here!"
+},
+{
+    "id": "13",
+    "firstResponse": "Olive",
+    "secondResponse": "Olive you. Do you love me too?"
+},
+{
+    "id": "14",
+    "firstResponse": "Mikey",
+    "secondResponse": "Mikey doesn’t work so help me out, would you?"
+},
+{
+    "id": "15",
+    "firstResponse": "Dwayne",
+    "secondResponse": "Dwayne the bathtub already. I’m drowning!"
+},
+{
+    "id": "16",
+    "firstResponse": "Ice cream",
+    "secondResponse": "Ice cream if you don't let me in."
+},
+{
+    "id": "17",
+    "firstResponse": "Alec",
+    "secondResponse": "Alec-tricity. Ain’t that a shocker?"
+},
+{
+    "id": "18",
+    "firstResponse": "Howl",
+    "secondResponse": "Howl you know unless you open the door?"
+},
+{
+    "id": "19",
+    "firstResponse": "Iran",
+    "secondResponse": "Iran all the way here."
+},
+{
+    "id": "20",
+    "firstResponse": "Snow",
+    "secondResponse": "Snow use askin’"
+},
+{
+    "id": "21",
+    "firstResponse": "Needle",
+    "secondResponse": "Needle little help gettin’ through."
+},
+{
+    "id": "22",
+    "firstResponse": "Thermos",
+    "secondResponse": "Thermos be a better way to get through to you."
+},
+{
+    "id": "23",
+    "firstResponse": "Water",
+    "secondResponse": "Water you doing? Just open the door!"
+},
+{
+    "id": "24",
+    "firstResponse": "Amos",
+    "secondResponse": "A mosquito"
+},
+{
+    "id": "25",
+    "firstResponse": "Dozen",
+    "secondResponse": "Dozen anyone wanna let me in?"
+},
+{
+    "id": "26",
+    "firstResponse": "Nanna",
+    "secondResponse": "Nanna your business, that’s who"
+},
+{
+    "id": "27",
+    "firstResponse": "Euripides",
+    "secondResponse": "Euripides jeans, you pay for ’em, okay?"
+},
+{
+    "id": "28",
+    "firstResponse": "Justin",
+    "secondResponse": "You re justin time for a miracle"
+},
+{
+    "id": "29",
+    "firstResponse": "Parton",
+    "secondResponse": "Parton my French!"
+},
+{
+    "id": "30",
+    "firstResponse": "Dover",
+    "secondResponse": "Ben Dover and I’ll give you a big surprise"
+},
+{
+    "id": "31",
+    "firstResponse": "Butch, Jimmy, and Joe",
+    "secondResponse": "Butch your arms around me, Jimmy a big hot kiss, and let’s Joe!"
+},
+{
+    "id": "32",
+    "firstResponse": "King Henry the Second",
+    "secondResponse": "King Henry, the second the queen leaves, we’ll bring in the strippers"
+},
+{
+    "id": "33",
+    "firstResponse": "Tex",
+    "secondResponse": "Tex two to tango!"
+},
+{
+    "id": "34",
+    "firstResponse": "Sizzle!",
+    "secondResponse": "Sizzle hurt me more than it will hurt you!"
+},
+{
+    "id": "35",
+    "firstResponse": "Its",
+    "secondResponse": "It's not me, it's you!"
+},
+{
+    "id": "36",
+    "firstResponse": "Some",
+    "secondResponse": "Some asshole talking to a knock knock joke"
+},
+{
+    "id": "37",
+    "firstResponse": "Phil",
+    "secondResponse": "Phil McCrackin!"
+},
+{
+    "id": "38",
+    "firstResponse": "Ivanna Seymour",
+    "secondResponse": "Ivanna Seymour Butts"
+},
+{
+    "id": "39",
+    "firstResponse": "Pauline",
+    "secondResponse": "I think I’m Pauline in love with you."
+},
+{
+    "id": "40",
+    "firstResponse": "Honeydew",
+    "secondResponse": "Honeydew you know how fine you look right now?"
+},
+{
+    "id": "41",
+    "firstResponse": "Egg",
+    "secondResponse": "Eggcited to meet you."
+},
+{
+    "id": "42",
+    "firstResponse": "Candice",
+    "secondResponse": "Candice be love I’m feeling right now?"
+},
+{
+    "id": "43",
+    "firstResponse": "Al",
+    "secondResponse": "Al give you a kiss if you open this door."
+},
+{
+    "id": "44",
+    "firstResponse": "Mirra",
+    "secondResponse": "Mirra Mirra on the wall"
+},
+{
+    "id": "45",
+    "firstResponse": "Cheese",
+    "secondResponse": "Cheese a cute girl."
+},
+{
+    "id": "46",
+    "firstResponse": "Marry",
+    "secondResponse": "Marry me"
+},
+{
+    "id": "47",
+    "firstResponse": "Aadrvark",
+    "secondResponse": "Aadrvark a hundred miles for one of your smile."
+},
+{
+    "id": "48",
+    "firstResponse": "Disguise",
+    "secondResponse": "This guy is your boyfriend"
+},
+{
+    "id": "49",
+    "firstResponse": "Howard",
+    "secondResponse": "Howard you like a big kiss?"
+},
+{
+    "id": "50",
+    "firstResponse": "Kiss",
+    "secondResponse": "Me."
+},
+{
+    "id": "51",
+    "firstResponse": "Police",
+    "secondResponse": "Police, it’s cold out here."
+},
+{
+    "id": "52",
+    "firstResponse": "Mikey",
+    "secondResponse": "Mikey doesn’t fit in the keyhole."
+},
+{
+    "id": "53",
+    "firstResponse": "Mayer",
+    "secondResponse": "Mayer days be filled with laughter and love"
+},
+{
+    "id": "54",
+    "firstResponse": "Needle",
+    "secondResponse": "Needle a little love right now"
+},
+{
+    "id": "55",
+    "firstResponse": "Sherwood",
+    "secondResponse": "Sherwood like to be your valentine."
+},
+{
+    "id": "56",
+    "firstResponse": "Juno",
+    "secondResponse": "Juno I love you, don’t you?"
+},
+{
+    "id": "57",
+    "firstResponse": "Do U Want 2 C Deez",
+    "secondResponse": "Do U Want 2 C Deez Nutz?"
+},
+{
+    "id": "58",
+    "firstResponse": "Howie",
+    "secondResponse": "Howie gonna hide this dead body?"
+},
+{
+    "id": "59",
+    "firstResponse": "Urine",
+    "secondResponse": "Urine secure, don't know what for"
+},
+{
+    "id": "60",
+    "firstResponse": "Urine",
+    "secondResponse": "Urine my heart!"
+},
+{
+    "id": "61",
+    "firstResponse": "Yo mama",
+    "secondResponse": "Yo mama behind you"
+},
+{
+    "id": "62",
+    "firstResponse": "Oink oink",
+    "secondResponse": "Make up your mind—are you a pig, or an owl?"
+},
+{
+    "id": "63",
+    "firstResponse": "Gladys",
+    "secondResponse": "Gladys the weekend"
+},
+{
+    "id": "64",
+    "firstResponse": "Wooden shoe",
+    "secondResponse": "Wooden shoe like to know!"
+},
+{
+    "id": "65",
+    "firstResponse": "Figs",
+    "secondResponse": "Figs the doorbell, it’s broken!"
+},
+{
+    "id": "66",
+    "firstResponse": "Snow",
+    "secondResponse": "Snow laughing matter"
+},
+{
+    "id": "67",
+    "firstResponse": "Wire",
+    "secondResponse": "Wire you always asking ‘who’s there’?"
+},
+{
+    "id": "68",
+    "firstResponse": "Leaf",
+    "secondResponse": "Leaf Me Alone!"
+},
+{
+    "id": "69",
+    "firstResponse": "Radio",
+    "secondResponse": "Radio not, here I come!"
+},
+{
+    "id": "70",
+    "firstResponse": "Cargo",
+    "secondResponse": "Car go beep, beep"
+},
+{
+    "id": "71",
+    "firstResponse": "Owls say",
+    "secondResponse": "Yes, they do!"
+},
+{
+    "id": "72",
+    "firstResponse": "Kanga who",
+    "secondResponse": "Actually, it’s kangaroo."
+},
+{
+    "id": "73",
+    "firstResponse": "Beets",
+    "secondResponse": "Beets me!"
+},
+{
+    "id": "74",
+    "firstResponse": "Pecan",
+    "secondResponse": "Pecan somebody your own size"
+},
+{
+    "id": "75",
+    "firstResponse": "Ho-ho",
+    "secondResponse": "Hoe Hoe You!"
+},
+{
+    "id": "76",
+    "firstResponse": "Ho-ho",
+    "secondResponse": "You know, your Santa impression could use a little work."
+},    
+{
+    "id": "77",
+    "firstResponse": "Hanna",
+    "secondResponse": "…Hanna partridge in a pear tree!"
+},    {
+    "id": "78",
+    "firstResponse": "Snow",
+    "secondResponse": "Snow use. I forgot my name again!"
+},    {
+    "id": "79",
+    "firstResponse": "Icing",
+    "secondResponse": "Icing so loud, the neighbors can hear"
+},    {
+    "id": "80",
+    "firstResponse": "Alpaca",
+    "secondResponse": "Alpaca the trunk, you pack the suitcase."
+},    {
+    "id": "81",
+    "firstResponse": "Yukon",
+    "secondResponse": "Yukon say that again!"
+},    {
+    "id": "82",
+    "firstResponse": "Cereal",
+    "secondResponse": "Cereal pleasure to meet you."
+},    {
+    "id": "83",
+    "firstResponse": "Bruce",
+    "secondResponse": "I Bruce easily, don't hit me !"
+},    {
+    "id": "84",
+    "firstResponse": "CD",
+    "secondResponse": "CD guy on your doorstep."
+},    {
+    "id": "85",
+    "firstResponse": "Cows go",
+    "secondResponse": "No, silly. Cows go moo."
+},    {
+    "id": "86",
+    "firstResponse": "Dishes",
+    "secondResponse": "Dishes the Police come out with your hands up!"
+},    {
+    "id": "87",
+    "firstResponse": "Ferdie",
+    "secondResponse": "Ferdie last time open this door!"
+},    {
+    "id": "88",
+    "firstResponse": "Ivor",
+    "secondResponse": "Ivor you let me in or I'll climb through the window!"
+},    {
+    "id": "89",
+    "firstResponse": "Luke",
+    "secondResponse": "Luke through the the peep hole and find out!"
+},    {
+    "id": "90",
+    "firstResponse": "Hatch",
+    "secondResponse": "God bless you."
+}
+]
+
+var firstErrors = [
+    {id: 1, error: "You have to ask who is at the door"},
+    {id: 2, error: "Fun Fact: You can ask 'Who dey there' or 'Who be that?'"},
+    {id: 3, error: "I am trying to tell a knock knock joke, find out who is knocking!"},
+    {id: 4, error: "Someone's at the door...of your heart...hehehe"}
+]
+
+var clientInput = document.getElementById('clientInput');
+var loader = document.getElementsByClassName('preloader')
+var off = document.getElementById('off');
+var on = document.getElementById('on');
+var chat = document.getElementById('chat')
+var navbarBrand = document.getElementsByClassName('navbar-brand')
+var clientResponse1 = ''
+var response = 0
+var selectedMsg = {}
+var selectedErr = {}
+var sound = 1
+var askedQs = []
+
+
+function knock(){
+    //Choose random joke
+    var randKnock = function getRandomInt(max) {
+        return Math.floor(Math.random() * Math.floor(max));
+    }
+    var selectedJokes = randKnock(jokes.length)
+    
+    if(askedQs.includes(selectedJokes)){
+        selectedJokes = randKnock(jokes.length);
+    }else{
+        askedQs.push(selectedJokes);
+        selectedMsg = jokes[selectedJokes]
+    }
+  
+    //Remove loader
+    loader[0].style.display = 'none'
+    //Intro Knock
+    var node = document.createElement("div");
+    var createBreak = document.createElement("br");
+    node.className = 'adaBot'
+    var textnode = document.createTextNode('Knock Knock');
+    node.appendChild(textnode);
+    chat.appendChild(node);
+    chat.appendChild(createBreak);
+    chat.scrollTop = chat.scrollHeight - chat.clientHeight;
+    if (sound == 1){
+        document.getElementById('audio').play();
+    }
+
+    // Listen for input
+    
+    clientInput.addEventListener('change',  function(){
+        clientResponse1 = clientInput.value
+    })
+   
+}
+
+//Listen for an enter key response
+function enterpressalert(e){
+    clientResponse1 = e.target.value
+    var code = (e.keyCode ? e.keyCode : e.which);
+        if(code == 13) { 
+            sendMsg()
+        }
+}
+
+function sendMsg(){
+    var node = document.createElement("div");
+    var createBreak = document.createElement("br");
+    node.className = 'youRespond'
+    var textnode = document.createTextNode(clientResponse1);
+    node.appendChild(textnode);
+    chat.appendChild(node);
+    chat.appendChild(createBreak);
+    clientInput.value = ''  
+    setTimeout(loading, 2000)
+    chat.scrollTop = chat.scrollHeight - chat.clientHeight;
+    if (sound == 1){
+        document.getElementById('sentAudio').play();
+    }
+    
+    
+}
+
+function loading(){
+    var node = document.createElement("div");
+    var createBreak = document.createElement("br");
+    node.id = 'loader'
+    var elips1 = document.createElement("div");
+    elips1.className = "lds-ellipsis"
+    var elips2 = document.createElement("div");
+    var elips3 = document.createElement("div");
+    var elips4 = document.createElement("div");
+    elips1.appendChild(elips2)
+    elips1.appendChild(elips3)
+    elips1.appendChild(elips4)
+    node.appendChild(elips1);
+    chat.appendChild(node);
+    chat.appendChild(createBreak);
+    chat.scrollTop = chat.scrollHeight - chat.clientHeight;
+    setTimeout(adahresponds, 2000);
+
+}
+
+function soundControl(value){
+    sound = value;
+    if(value == 0){
+        if (off.classList.contains("toggleinactive") && on.classList.contains("toggleBtn")) {
+        off.classList.add('toggleBtn');
+        off.classList.remove('toggleinactive');
+        on.classList.add('toggleinactive');
+        on.classList.remove('toggleBtn');
+        }
+    }else{
+        if (off.classList.contains("toggleBtn") && on.classList.contains("toggleinactive")) {
+        on.classList.add('toggleBtn');
+        on.classList.remove('toggleinactive');
+        off.classList.remove('toggleBtn');
+        off.classList.add('toggleinactive');
+        }
+    }
+}
+
+
+function adahresponds(){
+    var element = document.getElementById('loader');
+    element.parentNode.removeChild(element);
+  
+    var newString = clientResponse1.toLowerCase() 
+    var secondQs = selectedMsg.firstResponse + ' who';
+    var secondQs2 = selectedMsg.firstResponse + ' who?'
+    newString = newString.replace(/\s+/g, " ");
+ 
+    //Choose random error
+    var randError = function getRandomInt(max) {
+        return Math.floor(Math.random() * Math.floor(max));
+    }
+
+    var error = randError(firstErrors.length)
+    selectedErr = firstErrors[error]
+
+
+    //If it's the first response and it starts with 'who'
+    if(newString.startsWith("who") & response === 0){
+        var node = document.createElement("div");
+        var createBreak = document.createElement("br");
+        node.className = 'adaBot'
+        var textnode = document.createTextNode(selectedMsg.firstResponse);
+        node.appendChild(textnode);
+        chat.appendChild(node);
+        chat.appendChild(createBreak);
+        chat.scrollTop = chat.scrollHeight - chat.clientHeight;
+        if (sound === 1){
+            document.getElementById('audio').play();
+        }
+        response = 1
+        
+               
+    }
+    //If it's the second response and it starts with bot response and ends with who
+    else if(newString === secondQs.toLowerCase() || newString === secondQs2.toLowerCase()  & response === 1){
+        var node = document.createElement('div');
+        var createBreak = document.createElement("br");
+        node.className = 'adaBot'
+        var textnode = document.createTextNode(selectedMsg.secondResponse);
+        node.appendChild(textnode);
+        chat.appendChild(node);
+        chat.appendChild(createBreak)
+        chat.scrollTop = chat.scrollHeight - chat.clientHeight;
+        if (sound === 1){
+            document.getElementById('audio').play();
+        }
+        response = 0
+        setTimeout(knock, 2000) 
+    } 
+    //If they don't ask who is at the dootr
+    else if(!newString.startsWith("who") & response === 0){
+        var node = document.createElement("div");
+        var createBreak = document.createElement("br");
+        node.className = 'adaBot'
+        var textnode = document.createTextNode(selectedErr.error);
+        node.appendChild(textnode);
+        chat.appendChild(node);
+        chat.appendChild(createBreak)
+        chat.scrollTop = chat.scrollHeight - chat.clientHeight;
+        setTimeout(knock, 2000) 
+        if (sound == 1){
+            document.getElementById('audio').play();
+        }
+       
+        response = 0
+    }   
+    else{
+        //Error statement
+        var node = document.createElement("div");
+        var createBreak = document.createElement("br");
+        node.className = 'adaBot'
+        var textnode = document.createTextNode('You have to ask ' + selectedMsg.firstResponse + ' who'  );
+        node.appendChild(textnode);
+        chat.appendChild(node);
+        chat.appendChild(createBreak);
+        chat.scrollTop = chat.scrollHeight - chat.clientHeight;
+        setTimeout(knock, 2000) 
+        if (sound == 1){
+            document.getElementById('audio').play();
+        }
+        response = 0
+    }
+}
